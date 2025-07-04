354fe6eb1be1e0a0627e34cd55b11965739f1467
FabricioPRZ <233394@ids.upchiapas.edu.mx>
2025-07-04 00:28:00 -0600

    feat: add header component and integrate into login and register modules
    - Created HeaderComponent with HTML and SCSS styles.
    - Integrated HeaderComponent into LoginComponent and RegisterComponent.
    - Developed login and registration forms with appropriate styling.
    - Added unit tests for HeaderComponent, LoginComponent, and RegisterComponent.

---
+++ src/app/modules/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-register',
+  imports: [HeaderComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
