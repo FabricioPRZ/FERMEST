960d394e57314b4d631b908426d984bb0e6dfe7d
FabricioPRZ <233394@ids.upchiapas.edu.mx>
2025-07-28 04:35:08 -0600

    wss services

--- src/app/Docente/services/notification.service.ts
+++ src/app/Docente/services/notification.service.ts
@@ -22,7 +22,7 @@ export class NotificationService1 {
 
     this.isConnecting = true;
     this.ws = new WebSocket(
-      `wss://fermest-ws.it2id.cc/ws?user_id=${this.userId}&session_id=${this.sessionId}&code=${this.code}`
+      `wss://fermest-ws.it2id.cc:8082/ws?user_id=${this.userId}&session_id=${this.sessionId}&code=${this.code}`
     );
 
     this.ws.onopen = () => {
